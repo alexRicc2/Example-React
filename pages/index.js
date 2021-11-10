87249517cfc83cbd5131cb20c89faf82df7240a1
Alex Ricardo Rodrigues Santanna <81180853+alexRicc2@users.noreply.github.com>
2021-11-10 20:14:03 -0300

    adsense

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7042999523953127"
+     crossorigin="anonymous"></script>
       </Head>
     Hello world
     </div>
